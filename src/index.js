3c7c9d54571aa4bb164a2c99a706991ec1198014
Guy Lepage <guylepage3@gmail.com>
2018-10-10 10:47:52 -0400

    created new custom bootstrap saass theme file #23
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/Custom.scss';
 import './styles/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
