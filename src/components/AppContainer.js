39fe34652ffe5c4ec2fcf2ad9bafde637f6175d9
Guy Lepage <guylepage3@gmail.com>
2018-10-12 15:46:38 -0400

    import router components, set browser route #29
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

---
+++ src/components/AppContainer.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+class AppContainer extends Component {
+  render() {
+    return (
+      <div className="d-flex w-100 h-100 mx-auto p-4 flex-column">
+        <Header />
+        <div 
+          style={{
+            paddingTop: '40px'
+          }}>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default AppContainer;
