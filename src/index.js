5c73500add8b594299f4ad827946993b540600c4
Guy Lepage <guylepage3@gmail.com>
2018-10-13 11:58:01 -0400

    update to app syntax
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>, 
+    <App />,
+  </BrowserRouter>
   document.getElementById('root')
 );
 
