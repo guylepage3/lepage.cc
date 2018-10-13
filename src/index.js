3fc7224feb9c9fe9a1192d429f99ceee64c56699
Guy Lepage <guylepage3@gmail.com>
2018-10-13 12:32:16 -0400

    update to add react router test #29
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from "react-router-dom";
-// In order to have styles cascade correctly, styles must be imported 
-// before App.js as App.js has a style sheet to be imported last
+import { BrowserRouter } from 'react-router-dom';
 import './styles/Custom.scss'; 
 import './styles/index.css'; 
 import App from './App';
@@ -10,7 +8,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Route path="/" component={App} />
+    <App />
   </BrowserRouter>,
   document.getElementById('root')
 );
