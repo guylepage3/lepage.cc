ba849e03b03f0d05aaf3c01b063abdd93a8a7141
Guy Lepage <guylepage3@gmail.com>
2018-10-13 12:46:27 -0400

    repositioned app styles #29
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './styles/Custom.scss'; 
-import './styles/index.css'; 
+import './styles/Custom.scss';
+import './styles/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
