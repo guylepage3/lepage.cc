6d36f53035df5bfc920e0bb580dc07cc37993a05
Guy Lepage <guylepage3@gmail.com>
2018-10-13 11:22:50 -0400

    saved file
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

---
+++ src/components/Intro.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import Hero from './Hero';
+import Footer from './Footer';
+
+class Intro extends Component {
+  render() {
+    return (
+      <div className="d-flex w-100 h-100 mx-auto p-4 flex-column">
+        <Header />
+        <div 
+          style={{
+            paddingTop: '40px'
+          }}>
+        </div>
+        <Hero />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Intro;
