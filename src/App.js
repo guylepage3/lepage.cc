65a6e7a91d416038493a31cda2b19a6bd99e4ccd
Guy Lepage <guylepage3@gmail.com>
2018-10-14 11:49:46 -0400

    added everything for https redirect closes #35
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch } from "react-router-dom";
+import HttpsRedirect from 'react-https-redirect';
 import './styles/App.css';
 import Intro from './components/Intro';
 import Home from './components/Home';
@@ -9,12 +10,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Switch>
-          <Route exact path="/" component={Intro} />
-          <Route path="/Home" component={Home} />
-          {/* when none of the above match, <NoMatch> will be rendered */}
-          <Route component={NoMatch} />
-        </Switch>
+        <HttpsRedirect>
+          <Switch>
+            <Route exact path="/" component={Intro} />
+            <Route path="/Home" component={Home} />
+            {/* when none of the above match, <NoMatch> will be rendered */}
+            <Route component={NoMatch} />
+          </Switch>
+        </HttpsRedirect>
       </div>
     );
   }
