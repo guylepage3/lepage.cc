765fb6b3f85e07718af373dc6973e7784bfe92a5
Guy Lepage <guylepage3@gmail.com>
2018-10-20 19:17:51 -0400

    update to copy
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,28 +6,30 @@ class Hero extends Component {
       <main 
         role="main" 
         className="
+          container
           mx-auto 
           text-center 
           mt-auto 
           mb-auto 
           anima-slidein
-        "
-        style={{
-          maxWidth: '685px'
-        }}>
-        <h1 
-          className="
-            text-white 
-            font-weight-bold 
-            font-italic 
-            anima-fadein 
-            hero-quote
-          ">
-          "Less, but better."
-        </h1>
-        <div className="lead text-white">
-          <div className="anima-fadein anima-fadein-delay-1700">
-            <i>~ Dieter Rams</i>
+        ">
+        <div className="row">
+          <div className="container-fluid">
+            <h1 
+              className="
+                text-white 
+                font-weight-bold 
+                font-italic 
+                anima-fadein 
+                hero-quote
+              ">
+              "Less, but better."
+            </h1>
+          </div>
+          <div className="container-fluid lead text-white">
+            <div className="anima-fadein anima-fadein-delay-1700">
+              <i>~ Dieter Rams</i>
+            </div>
           </div>
         </div>
       </main>
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,36 +6,34 @@ class Landing extends Component {
       <main 
         role="main" 
         className="
-          Content 
+          container 
           mb-auto 
-          mt-auto
-        ">        
-        <div 
-          className="
-            mx-auto 
-            anima-slidein
-          "
-          style={{
-            maxWidth: '685px'
-          }}>
-          <h1 
-            className="
-              text-white 
-              font-weight-bold 
-              font-italic 
-              anima-fadein 
-              hero-quote
-            ">
-            Simple, clean, concise experiences that keep the user coming back, 
-            time and time again.
-          </h1>
-          <div className="lead text-white">
-            <div className="anima-fadein">
-              I'm Guy Lepage, a Senior Product UX/UI Designer and Front-End 
-              Developer with 20 years of industry experience, specializing in 
-              designing and developing pixel perfect digital products, web 
-              apps, websites, etc. quickly without sacrificing code quality 
-              and ensuring that users have the best experience.
+          mt-auto 
+          mx-auto 
+          anima-slidein
+        ">
+        <div className="row">
+          <div className="col-lg-6 mx-auto">
+            <h1 
+              className="
+                text-white 
+                font-weight-bold 
+                font-italic 
+                anima-fadein 
+                hero-quote
+              ">
+              Simple, clean, concise experiences that keep the user coming back, 
+              time and time again.
+            </h1>
+            <div className="lead text-white">
+              <div className="anima-fadein">
+                I'm Guy Lepage, a Senior Product UX/UI Designer and Front-end 
+                Developer with 19 years of industry experience, specializing 
+                in designing and developing pixel perfect digital products, 
+                web apps, mobile apps, websites, and more. My focus is on 
+                ensuring that users have the best experiences without 
+                sacrificing code quality.
+              </div>
             </div>
           </div>
         </div>
