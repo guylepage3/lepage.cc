75e9167bdcfc24cd0e3aef7967f9c629e95ea458
Guy Lepage <guylepage3@gmail.com>
2018-11-06 08:05:11 -0800

    setup pattern library
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './styles/App.css';
 import Intro from './components/Intro';
 import Home from './components/Home';
 import NoMatch from './components/NoMatch';
+import PatternLib from './components/PatternLib';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={Intro} />
           <Route path="/Home" component={Home} />
+          <Route path="/pattern-library" component={PatternLib} />
           {/* when none of the above match, <NoMatch> will be rendered */}
           <Route component={NoMatch} />
         </Switch>
---
+++ src/components/PatternLib.js
@@ -0,0 +1,231 @@
+import React, { Component } from 'react';
+import HeaderLight from './HeaderLight';
+
+class PatternLib extends Component {
+  render() {
+    return (
+      <div>
+      <HeaderLight />
+      <div 
+        style={{
+          paddingTop: '57px',
+          backgroundColor: '#212121'
+        }}>
+      </div>
+      <main 
+        role="main" 
+        className="
+          container 
+          mb-auto 
+          mt-auto 
+          mx-auto 
+          anima-slidein
+        ">
+        <div className="row">
+          <div className="col-lg-9 col-xl-7 mx-auto">
+            <h1 className="text-white">
+              Duis ullamcorper feugiat sodales. Maecenas in nisl venenatis, volutpat nisl quis
+            </h1>
+            <p className="text-white">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel facilisis sapien. Vestibulum nec viverra magna, quis cursus lectus. Maecenas egestas elit nisi, a fringilla mi feugiat eget. Suspendisse sollicitudin tristique fringilla. Ut non risus nulla.
+            </p>
+            <p className="text-white">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel facilisis sapien. Vestibulum nec viverra magna, quis cursus lectus. Maecenas egestas elit nisi, a fringilla mi feugiat eget. Suspendisse sollicitudin tristique fringilla. Ut non risus nulla.
+            </p>
+
+            <div className="container typetemplate-boxy-line text-center"></div>
+            
+            <h2 className="text-white">
+              Aenean in velit ut ante congue euismod. Sed ligula enim, facilisis id posuere vel, feugiat non diam. Donec ullamcorper molestie maximus.
+            </h2>
+            <div className="container typetemplate-boxy-img-box"></div>
+            <p className="text-white">
+              Sed sapien erat, finibus vitae dolor eget, lobortis dignissim ante. Aliquam pharetra ac sem vitae convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed urna turpis, interdum quis venenatis in, porta nec mauris. Etiam aliquet nibh eu vulputate pellentesque.
+            </p>
+            <p className="text-white">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel facilisis sapien. Vestibulum nec viverra magna, quis cursus lectus. Maecenas egestas elit nisi, a fringilla mi feugiat eget. Suspendisse sollicitudin tristique fringilla. Ut non risus nulla.
+            </p>
+            <h2 className="text-white">
+              Praesent sodales felis pretium orci aliquet egestas. Nunc eget interdum augue.
+            </h2>
+            <p className="text-white">
+              Sed sapien erat, finibus vitae dolor eget, lobortis dignissim ante. Aliquam pharetra ac sem vitae convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed urna turpis, interdum quis venenatis in, porta nec mauris. Etiam aliquet nibh eu vulputate pellentesque.
+            </p>
+            <p className="text-white">
+              Think of variables as a way to store information that you want to reuse throughout your stylesheet. You can store things like colors, font stacks, or any CSS value you think you'll want to reuse. Sass uses the $ symbol to make something a variable
+            </p>
+            <p className="text-white">
+              Maecenas in rhoncus augue. Fusce diam purus, consequat non enim eget, feugiat dictum velit. Vestibulum ac tincidunt mi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent vulputate tortor quam, non pulvinar ante porta at. Maecenas finibus bibendum euismod. Maecenas vehicula nunc turpis, non dictum dui scelerisque non. In vulputate convallis porttitor. Cras fringilla diam dolor, eget commodo erat dapibus sed. Nunc in quam congue, aliquet nulla eget, gravida lacus. Quisque fringilla felis a lorem volutpat consequat. Nunc nibh neque, lobortis vel elementum vel, mattis ac neque.
+            </p>
+            <h2 className="text-white">
+              Sed suscipit mauris et quam sagittis, eget rhoncus diam lobortis. Integer nec eros magna. Duis non ipsum tincidunt, sodales nulla sed, tempor eros.
+            </h2>
+            <p className="text-white">
+              Sed sapien erat, finibus vitae dolor eget, lobortis dignissim ante. Aliquam pharetra ac sem vitae convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed urna turpis, interdum quis venenatis in, porta nec mauris. Etiam aliquet nibh eu vulputate pellentesque.
+            </p>
+            <h2 className="text-white">
+              Nulla nec lacus interdum diam sodales vestibulum. Aliquam laoreet non arcu vel posuere.
+            </h2>
+            <p className="text-white">
+              Sed sapien erat, finibus vitae dolor eget, lobortis dignissim ante. Aliquam pharetra ac sem vitae convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed urna turpis, interdum quis venenatis in, porta nec mauris. Etiam aliquet nibh eu vulputate pellentesque.
+            </p>
+
+            <div className="container typetemplate-boxy-line text-center"></div>
+            
+            <h4 className="text-white">
+              Aenean in velit ut ante congue euismod. Sed ligula enim, facilisis id posuere vel, feugiat non diam. Donec ullamcorper molestie maximus.
+            </h4>
+            <div className="container typetemplate-boxy-img-box"></div>
+            <p className="text-white">
+              Sed sapien erat, finibus vitae dolor eget, lobortis dignissim ante. Aliquam pharetra ac sem vitae convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed urna turpis, interdum quis venenatis in, porta nec mauris. Etiam aliquet nibh eu vulputate pellentesque.
+            </p>
+            <p className="text-white">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel facilisis sapien. Vestibulum nec viverra magna, quis cursus lectus. Maecenas egestas elit nisi, a fringilla mi feugiat eget. Suspendisse sollicitudin tristique fringilla. Ut non risus nulla.
+            </p>
+            <h4 className="text-white">
+              Praesent sodales felis pretium orci aliquet egestas. Nunc eget interdum augue.
+            </h4>
+            <p className="text-white">
+              Sed sapien erat, finibus vitae dolor eget, lobortis dignissim ante. Aliquam pharetra ac sem vitae convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed urna turpis, interdum quis venenatis in, porta nec mauris. Etiam aliquet nibh eu vulputate pellentesque.
+            </p>
+            <p className="text-white">
+              Think of variables as a way to store information that you want to reuse throughout your stylesheet. You can store things like colors, font stacks, or any CSS value you think you'll want to reuse. Sass uses the $ symbol to make something a variable
+            </p>
+            <p className="text-white">
+              Maecenas in rhoncus augue. Fusce diam purus, consequat non enim eget, feugiat dictum velit. Vestibulum ac tincidunt mi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent vulputate tortor quam, non pulvinar ante porta at. Maecenas finibus bibendum euismod. Maecenas vehicula nunc turpis, non dictum dui scelerisque non. In vulputate convallis porttitor. Cras fringilla diam dolor, eget commodo erat dapibus sed. Nunc in quam congue, aliquet nulla eget, gravida lacus. Quisque fringilla felis a lorem volutpat consequat. Nunc nibh neque, lobortis vel elementum vel, mattis ac neque.
+            </p>
+            <h4 className="text-white">
+              Sed suscipit mauris et quam sagittis, eget rhoncus diam lobortis. Integer nec eros magna. Duis non ipsum tincidunt, sodales nulla sed, tempor eros.
+            </h4>
+            <p className="text-white">
+              Sed sapien erat, finibus vitae dolor eget, lobortis dignissim ante. Aliquam pharetra ac sem vitae convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed urna turpis, interdum quis venenatis in, porta nec mauris. Etiam aliquet nibh eu vulputate pellentesque.
+            </p>
+            <h4 className="text-white">
+              Nulla nec lacus interdum diam sodales vestibulum. Aliquam laoreet non arcu vel posuere.
+            </h4>
+            <p className="text-white">
+              Sed sapien erat, finibus vitae dolor eget, lobortis dignissim ante. Aliquam pharetra ac sem vitae convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed urna turpis, interdum quis venenatis in, porta nec mauris. Etiam aliquet nibh eu vulputate pellentesque.
+            </p>
+
+            <div className="container typetemplate-boxy-line text-center"></div>
+
+
+            <h1 className="text-white">
+              Praesent sodales felis pretium orci aliquet egestas. Nunc eget interdum augue.
+            </h1>
+            <p className="text-white">
+              Sed sapien erat, finibus vitae dolor eget, lobortis dignissim ante. Aliquam pharetra ac sem vitae convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed urna turpis, interdum quis venenatis in, porta nec mauris. Etiam aliquet nibh eu vulputate pellentesque.
+            </p>
+            <h2 className="text-white">
+              Praesent sodales felis pretium orci aliquet egestas. Nunc eget interdum augue.
+            </h2>
+            <p className="text-white">
+              Sed sapien erat, finibus vitae dolor eget, lobortis dignissim ante. Aliquam pharetra ac sem vitae convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed urna turpis, interdum quis venenatis in, porta nec mauris. Etiam aliquet nibh eu vulputate pellentesque.
+            </p>
+            <h3 className="text-white">
+              Praesent sodales felis pretium orci aliquet egestas. Nunc eget interdum augue.
+            </h3>
+            <p className="text-white">
+              Sed sapien erat, finibus vitae dolor eget, lobortis dignissim ante. Aliquam pharetra ac sem vitae convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed urna turpis, interdum quis venenatis in, porta nec mauris. Etiam aliquet nibh eu vulputate pellentesque.
+            </p>
+            <h4 className="text-white">
+              Praesent sodales felis pretium orci aliquet egestas. Nunc eget interdum augue.
+            </h4>
+            <p className="text-white">
+              Sed sapien erat, finibus vitae dolor eget, lobortis dignissim ante. Aliquam pharetra ac sem vitae convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed urna turpis, interdum quis venenatis in, porta nec mauris. Etiam aliquet nibh eu vulputate pellentesque.
+            </p>
+            <h5 className="text-white">
+              Praesent sodales felis pretium orci aliquet egestas. Nunc eget interdum augue.
+            </h5>
+            <p className="text-white">
+              Sed sapien erat, finibus vitae dolor eget, lobortis dignissim ante. Aliquam pharetra ac sem vitae convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed urna turpis, interdum quis venenatis in, porta nec mauris. Etiam aliquet nibh eu vulputate pellentesque.
+            </p>
+            <h6 className="text-white">
+              Praesent sodales felis pretium orci aliquet egestas. Nunc eget interdum augue.
+            </h6>
+            <p className="text-white">
+              Sed sapien erat, finibus vitae dolor eget, lobortis dignissim ante. Aliquam pharetra ac sem vitae convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed urna turpis, interdum quis venenatis in, porta nec mauris. Etiam aliquet nibh eu vulputate pellentesque.
+            </p>
+
+            <div className="lead text-white">
+              Praesent sodales felis pretium orci aliquet egestas. Nunc eget interdum augue. Sed suscipit mauris et quam sagittis, eget rhoncus diam lobortis. Integer nec eros magna. Duis non ipsum tincidunt, sodales nulla sed, tempor eros.
+            </div>
+            <p className="text-white-50">
+              Sed sapien erat, finibus vitae dolor eget, lobortis dignissim ante. Aliquam pharetra ac sem vitae convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed urna turpis, interdum quis venenatis in, porta nec mauris. Etiam aliquet nibh eu vulputate pellentesque.
+            </p>
+
+            <div className="container typetemplate-boxy-line text-center"></div>
+
+
+            <h1 className="display-1 text-white">
+              Praesent sodales felis pretium orci aliquet egestas. Nunc eget interdum augue.
+            </h1>
+            <p className="text-white">
+              Sed sapien erat, finibus vitae dolor eget, lobortis dignissim ante. Aliquam pharetra ac sem vitae convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed urna turpis, interdum quis venenatis in, porta nec mauris. Etiam aliquet nibh eu vulputate pellentesque.
+            </p>
+            <h1 className="display-2 text-white">
+              Praesent sodales felis pretium orci aliquet egestas. Nunc eget interdum augue.
+            </h1>
+            <p className="text-white">
+              Sed sapien erat, finibus vitae dolor eget, lobortis dignissim ante. Aliquam pharetra ac sem vitae convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed urna turpis, interdum quis venenatis in, porta nec mauris. Etiam aliquet nibh eu vulputate pellentesque.
+            </p>
+            <h1 className="display-3 text-white">
+              Praesent sodales felis pretium orci aliquet egestas. Nunc eget interdum augue.
+            </h1>
+            <p className="text-white">
+              Sed sapien erat, finibus vitae dolor eget, lobortis dignissim ante. Aliquam pharetra ac sem vitae convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed urna turpis, interdum quis venenatis in, porta nec mauris. Etiam aliquet nibh eu vulputate pellentesque.
+            </p>
+            <h1 className="display-4 text-white">
+              Praesent sodales felis pretium orci aliquet egestas. Nunc eget interdum augue.
+            </h1>
+            <p className="text-white">
+              Sed sapien erat, finibus vitae dolor eget, lobortis dignissim ante. Aliquam pharetra ac sem vitae convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed urna turpis, interdum quis venenatis in, porta nec mauris. Etiam aliquet nibh eu vulputate pellentesque.
+            </p>
+
+            <div className="row">
+              <div className="col-6">
+                <div className="lead text-white">
+                  Praesent sodales felis pretium orci aliquet egestas. Nunc eget interdum augue. Sed suscipit mauris et quam sagittis, eget rhoncus diam lobortis. Integer nec eros magna. Duis non ipsum tincidunt, sodales nulla sed, tempor eros.
+                </div>
+                <p className="text-white-50">
+                  Sed sapien erat, finibus vitae dolor eget, lobortis dignissim ante. Aliquam pharetra ac sem vitae convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed urna turpis, interdum quis venenatis in, porta nec mauris. Etiam aliquet nibh eu vulputate pellentesque.
+                </p>
+              </div>
+              <div className="col-6">
+                <div className="lead text-white">
+                  Praesent sodales felis pretium orci aliquet egestas. Nunc eget interdum augue. Sed suscipit mauris et quam sagittis, eget rhoncus diam lobortis. Integer nec eros magna. Duis non ipsum tincidunt, sodales nulla sed, tempor eros.
+                </div>
+                <p className="text-white-50">
+                  Sed sapien erat, finibus vitae dolor eget, lobortis dignissim ante. Aliquam pharetra ac sem vitae convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed urna turpis, interdum quis venenatis in, porta nec mauris. Etiam aliquet nibh eu vulputate pellentesque.
+                </p>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-6">
+                <div className="lead text-white">
+                  Praesent sodales felis pretium orci aliquet egestas. Nunc eget interdum augue. Sed suscipit mauris et quam sagittis, eget rhoncus diam lobortis. Integer nec eros magna. Duis non ipsum tincidunt, sodales nulla sed, tempor eros.
+                </div>
+                <p className="text-white-50">
+                  Sed sapien erat, finibus vitae dolor eget, lobortis dignissim ante. Aliquam pharetra ac sem vitae convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed urna turpis, interdum quis venenatis in, porta nec mauris. Etiam aliquet nibh eu vulputate pellentesque.
+                </p>
+              </div>
+              <div className="col-6">
+                <div className="lead text-white">
+                  Praesent sodales felis pretium orci aliquet egestas. Nunc eget interdum augue. Sed suscipit mauris et quam sagittis, eget rhoncus diam lobortis. Integer nec eros magna. Duis non ipsum tincidunt, sodales nulla sed, tempor eros.
+                </div>
+                <p className="text-white-50">
+                  Sed sapien erat, finibus vitae dolor eget, lobortis dignissim ante. Aliquam pharetra ac sem vitae convallis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed urna turpis, interdum quis venenatis in, porta nec mauris. Etiam aliquet nibh eu vulputate pellentesque.
+                </p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </main>
+      </div>
+    );
+  }
+}
+
+export default PatternLib;
