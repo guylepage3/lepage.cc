f4d964f2db902d0e5339bbf295911ecc4e927cb0
Guy Lepage <guylepage3@gmail.com>
2018-11-23 18:37:17 -0500

    resolve token
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,14 +12,16 @@ class Footer extends Component {
               <a href="https://github.com/guylepage3/" 
                 alt="GitHub guylepage3 (Guy Lepage)"
                 className="text-white">
-                <FontAwesomeIcon className="mr-1" icon={['fab', 'github']} /> GitHub
+                <FontAwesomeIcon className="mr-1" icon={['fab', 'github']} /> 
+                  GitHub
               </a>
             </li>
             <li className="list-inline-item mx-2">
               <a href="https://www.npmjs.com/~guylepage3" 
                 alt="npm guylepage3 (Guy Lepage)"
                 className="text-white">
-                <FontAwesomeIcon className="mr-1" icon={['fab', 'npm']} /> npm
+                <FontAwesomeIcon className="mr-1" icon={['fab', 'npm']} /> 
+                  npm
               </a>
             </li>
             <li className="list-inline-item mx-2">
