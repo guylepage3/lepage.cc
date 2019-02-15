3f9e15b93ede70e966bd19d590b7655ebbe3409b
Guy Lepage <guylepage3@gmail.com>
2019-02-15 06:16:50 -0500

    switch version of jquery ui
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- src/components/NavbarToggler.js
+++ src/components/NavbarToggler.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
-import 'jquery-ui-dist/jquery-ui.min.js';
+import 'jquery-ui-dist/jquery-ui.js';
 
 $(document).ready( function() {
   $("button").click( function() {
