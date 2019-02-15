26e5cf26a852efb73a94dd81d86eb8fafda56e03
Guy Lepage <guylepage3@gmail.com>
2019-02-15 06:04:38 -0500

    import jquery ui
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- src/components/NavbarToggler.js
+++ src/components/NavbarToggler.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
-import 'jquery-ui-dist/jquery-ui.min.js';
+import 'jquery-ui-dist';
 
 $(document).ready( function() {
   $("button").click( function() {
