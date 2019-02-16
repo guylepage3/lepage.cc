bf6de06dfb621e626e4836c2df2bd9b757ba3d8d
Guy Lepage <guylepage3@gmail.com>
2019-02-15 19:32:52 -0500

    added navbar to intro
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- src/containers/Intro.js
+++ src/containers/Intro.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Splash from './Splash';
 
@@ -15,6 +16,7 @@ class Intro extends Component {
   render() {
     return (
       <div className="d-flex w-100 h-100 mx-auto flex-column">
+        <Navbar />
         <div 
           style={{
             paddingTop: '32px'
