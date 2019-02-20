07e0fe66c2c7f21aa00aefdbda807309b87b2ddf
Guy Lepage <guylepage3@gmail.com>
2019-02-20 09:43:49 -0500

    moved styles into component navline
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- client/src/components/NavLine.js
+++ client/src/components/NavLine.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Line = styled.div`
+  display: none;
+
+  @media (min-width: 768px) {
+    position: absolute;
+    display: block;
+    height: 1px;
+    width: 100%;
+    z-index: 1;
+  }
+
+  @media (min-width: 992px) {
+    position: absolute;
+    display: block;
+    height: 1px;
+    width: 100%;
+    left: 50%;
+    margin-right: 0;
+    margin-left: -480px;
+  }
+  
+  @media (min-width: 1200px) {
+    left: 50%;
+    margin-right: 0;
+    margin-left: -570px;
+  }
+`;
 
 class NavLine extends Component {
   render() {
     return (
-      <div className="container line bg-black">
-      </div>
+      <Line className="container line bg-black">
+      </Line>
     );
   }
 }
