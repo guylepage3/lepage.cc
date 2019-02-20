aaaabc2f531cd8a0487ce1ddfbd15d353ca8bacb
Guy Lepage <guylepage3@gmail.com>
2019-02-20 17:19:14 -0500

    resolved noreferrer issue
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- client/src/containers/About.js
+++ client/src/containers/About.js
@@ -31,60 +31,60 @@ class About extends Component {
             International award-winning senior product and UX/UI designer, marketing expert and entrepreneur with 19+ years experience working on some of the world’s largest brands and most exciting startups.
           </p>
           <p>
-          First hire at <a href="http://blockstack.org" alt="Blockstack" target="_blank" className="bg-black text-light px-1 mx-1">Blockstack</a>, a venture backed blockchain startup who has built a new decentralized internet for decentralized applications. Guy is an entrepreneur with a passion for business, design and technology. He is a former digital advertising agency business founder and is an internationally recognized designer. Guy took a non-traditional route and dropped out of College to pursue design and entrepreneurship. His career path has grown from a Newspaper Art Direction, to Creative Director with ad agencies and tech companies, to the pursuit and passion for software startups, design and technology.
+          First hire at <a href="http://blockstack.org" alt="Blockstack" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;Blockstack&nbsp;</a>, a venture backed blockchain startup who has built a new decentralized internet for decentralized applications. Guy is an entrepreneur with a passion for business, design and technology. He is a former digital advertising agency business founder and is an internationally recognized designer. Guy took a non-traditional route and dropped out of College to pursue design and entrepreneurship. His career path has grown from a Newspaper Art Direction, to Creative Director with ad agencies and tech companies, to the pursuit and passion for software startups, design and technology.
           </p>
           <h3 className="h3-editorial">
             Highlights
           </h3>
           <div>
             <ul>
-              <li>
-                Was the first hire at <a href="http://blockstack.org" alt="Blockstack" target="_blank" className="bg-black text-light px-1 mx-1">Blockstack</a>, a blockchain startup working closely with the founders and designed and built 25+ products, including the world's first decentralized web browser, chrome extensions, native apps, web apps, websites, and more... All completed over a 2.5 yr period.
+              <li style={{ marginBottom: '1rem' }}>
+                Was the first hire at <a href="http://blockstack.org" alt="Blockstack" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;Blockstack&nbsp;</a>, a blockchain startup working closely with the founders and designed and built 25+ products, including the world's first decentralized web browser, chrome extensions, native apps, web apps, websites, and more... All completed over a 2.5 yr period.
               </li>
-              <li>
-                Was one of the core contributors to <a href="https://venturebeat.com/2017/12/04/blockstack-raises-52-million-to-build-a-parallel-internet-where-you-own-all-your-data/" alt="Blockstack raises $52 million to build a parallel internet where you own all your data" target="_blank" className="bg-black text-light px-1 mx-1">Blockstack’s $52M ICO</a> and token distribution. VC’s who participated in the ICO include;
+              <li style={{ marginBottom: '1rem' }}>
+                Was one of the core contributors to <a href="https://venturebeat.com/2017/12/04/blockstack-raises-52-million-to-build-a-parallel-internet-where-you-own-all-your-data/" alt="Blockstack raises $52 million to build a parallel internet where you own all your data" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;Blockstack’s $52M ICO&nbsp;</a> and token distribution. VC’s who participated in the ICO include;
               </li>
-                <ul>
-                  <li><a href="http://usv.com" alt="Union Square Ventures (USV)" target="_blank" className="bg-black text-light px-1 mx-1">Union Square Ventures (USV)</a></li>
-                  <li><a href="http://foundationcapital.com/" alt="Foundation Capital" target="_blank" className="bg-black text-light px-1 mx-1">Foundation Capital</a></li>
-                  <li><a href="http://www.luxcapital.com/" alt="Lux Capital" target="_blank" className="bg-black text-light px-1 mx-1">Lux Capital</a></li>
-                  <li><a href="http://winklevosscapital.com/" alt="Winklevoss Capital" target="_blank" className="bg-black text-light px-1 mx-1">Winklevoss Capital</a></li>
-                  <li><a href="http://blockchain.capital/" alt="Blockchain Capital" target="_blank" className="bg-black text-light px-1 mx-1">Blockchain Capital</a></li>
-                  <li><a href="http://dcg.co/" alt="Digital Currency Group" target="_blank" className="bg-black text-light px-1 mx-1">Digital Currency Group</a></li>
-                  <li><a href="http://twitter.com/@kevinrose" alt="Kevin Rose" target="_blank" className="bg-black text-light px-1 mx-1">Kevin Rose</a></li>
-                  <li><a href="http://qasaryounis.com/" alt="Qasar Younis" target="_blank" className="bg-black text-light px-1 mx-1">Qasar Younis</a> former COO of <a href="http://www.ycombinator.com/" alt="Y Combinator" target="_blank" className="bg-black text-light px-1 mx-1">Y Combinator</a></li>
+                <ul style={{ marginTop: '0!important', marginBottom: '1rem' }}>
+                  <li><a href="http://usv.com" alt="Union Square Ventures (USV)" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;Union Square Ventures (USV)&nbsp;</a></li>
+                  <li><a href="http://foundationcapital.com/" alt="Foundation Capital" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;Foundation Capital&nbsp;</a></li>
+                  <li><a href="http://www.luxcapital.com/" alt="Lux Capital" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;Lux Capital&nbsp;</a></li>
+                  <li><a href="http://winklevosscapital.com/" alt="Winklevoss Capital" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;Winklevoss Capital&nbsp;</a></li>
+                  <li><a href="http://blockchain.capital/" alt="Blockchain Capital" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;Blockchain Capital&nbsp;</a></li>
+                  <li><a href="http://dcg.co/" alt="Digital Currency Group" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;Digital Currency Group&nbsp;</a></li>
+                  <li><a href="http://twitter.com/@kevinrose" alt="Kevin Rose" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;Kevin Rose&nbsp;</a></li>
+                  <li><a href="http://qasaryounis.com/" alt="Qasar Younis" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;Qasar Younis&nbsp;</a> former COO of <a href="http://www.ycombinator.com/" alt="Y Combinator" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;Y Combinator&nbsp;</a></li>
                 </ul>
-              <li>
-                Assisted in the raising of a <a href="https://techcrunch.com/2017/01/05/blockstack-formerly-called-onename-just-raised-4-million/" alt="Blockstack, formerly called Onename, just raised $4 million" target="_blank" className="bg-black text-light px-1 mx-1">$4M Series A funding round</a> led by <a href="http://usv.com" alt="Union Square Ventures (USV)" target="_blank" className="bg-black text-light px-1 mx-1">Union Square Ventures (USV)</a>. Other investors included;
+              <li style={{ marginBottom: '1rem' }}>
+                Assisted in the raising of a <a href="https://techcrunch.com/2017/01/05/blockstack-formerly-called-onename-just-raised-4-million/" alt="Blockstack, formerly called Onename, just raised $4 million" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;$4M Series A funding round&nbsp;</a> led by <a href="http://usv.com" alt="Union Square Ventures (USV)" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;Union Square Ventures (USV)&nbsp;</a>. Other investors included;
               </li>
-                <ul>
-                  <li><a href="http://www.luxcapital.com/" alt="Lux Capital" target="_blank" className="bg-black text-light px-1 mx-1">Lux Capital</a></li>
-                  <li><a href="http://dcg.co/" alt="Digital Currency Group" target="_blank" className="bg-black text-light px-1 mx-1">Digital Currency Group</a></li>
-                  <li><a href="http://compound.vc/" alt="Compound VC" target="_blank" className="bg-black text-light px-1 mx-1">Compound VC</a></li>
-                  <li><a href="http://versionone.vc/" alt="Version One Ventures" target="_blank" className="bg-black text-light px-1 mx-1">Version One Ventures</a></li>
-                  <li><a href="http://twitter.com/kalvepuri" alt="Kal Vepuri" target="_blank" className="bg-black text-light px-1 mx-1">Kal Vepuri</a></li>
-                  <li><a href="http://rtf.vc/" alt="Rising Tide" target="_blank" className="bg-black text-light px-1 mx-1">Rising Tide</a></li>
-                  <li><a href="https://twitter.com/naval" alt="Naval Ravikant (earlier investor)" target="_blank" className="bg-black text-light px-1 mx-1">Naval Ravikant (earlier investor)</a></li>
+                <ul style={{ marginTop: '0!important', marginBottom: '1rem' }}>
+                  <li><a href="http://www.luxcapital.com/" alt="Lux Capital" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;Lux Capital&nbsp;</a></li>
+                  <li><a href="http://dcg.co/" alt="Digital Currency Group" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;Digital Currency Group&nbsp;</a></li>
+                  <li><a href="http://compound.vc/" alt="Compound VC" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;Compound VC&nbsp;</a></li>
+                  <li><a href="http://versionone.vc/" alt="Version One Ventures" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;Version One Ventures&nbsp;</a></li>
+                  <li><a href="http://twitter.com/kalvepuri" alt="Kal Vepuri" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;Kal Vepuri&nbsp;</a></li>
+                  <li><a href="http://rtf.vc/" alt="Rising Tide" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;Rising Tide&nbsp;</a></li>
+                  <li><a href="https://twitter.com/naval" alt="Naval Ravikant" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;Naval Ravikant&nbsp;</a> (earlier investor)</li>
                 </ul>
-              <li>
+              <li style={{ marginBottom: '1rem' }}>
                 Assisted in growing the core blockchain user base from 35,000 to 75,000 users.
               </li>
-              <li>
+              <li style={{ marginBottom: '1rem' }}>
                 Was a key contributor in building a community of blockchain developers from 0 to 13,000 community members.
               </li>
-              <li>
-                Guy sits on the Board of Advisors to <a href="https://news.net-cents.com/2018/03/21/blockstacks-guy-lepage-joins-netcents-technologys-board-advisors/" alt="NetCents Technology" target="_blank" className="bg-black text-light px-1 mx-1">NetCents Technology</a>, a publicly traded tech company trading in <a href="https://www.bloomberg.com/quote/NC:CN" alt="NC:Canadian Sec Stock Quote - NetCents Technology Inc - Bloomberg Markets" target="_blank" className="bg-black text-light px-1 mx-1">Canada</a> and <a href="https://www.bloomberg.com/quote/26N:GR" alt="26N:Frankfurt Stock Quote - NetCents Technology Inc - Bloomberg Markets" target="_blank" className="bg-black text-light px-1 mx-1">Germany</a>.
+              <li style={{ marginBottom: '1rem' }}>
+                Guy sits on the Board of Advisors to <a href="https://news.net-cents.com/2018/03/21/blockstacks-guy-lepage-joins-netcents-technologys-board-advisors/" alt="NetCents Technology" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;NetCents Technology&nbsp;</a>, a publicly traded tech company trading in <a href="https://www.bloomberg.com/quote/NC:CN" alt="NC:Canadian Sec Stock Quote - NetCents Technology Inc - Bloomberg Markets" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;Canada&nbsp;</a> and <a href="https://www.bloomberg.com/quote/26N:GR" alt="26N:Frankfurt Stock Quote - NetCents Technology Inc - Bloomberg Markets" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;Germany&nbsp;</a>.
               </li>
-              <li>
-                Designed a the worlds first decentralized web browser that was a <a href="https://twitter.com/guylepage3/status/889857953980964864" alt="Blockstack. A reinvention of the Internet for better privacy" target="_blank" className="bg-black text-light px-1 mx-1">finalist in the 2017 INDEX awards</a>.
+              <li style={{ marginBottom: '1rem' }}>
+                Designed a the worlds first decentralized web browser that was a <a href="https://twitter.com/guylepage3/status/889857953980964864" alt="Blockstack. A reinvention of the Internet for better privacy" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;finalist in the 2017 INDEX awards&nbsp;</a>.
               </li>
-              <li>
+              <li style={{ marginBottom: '1rem' }}>
                 Part of the New York and Silicon Valley tech startup community.
               </li>
-              <li>
-                In 2014, <a href="http://bit.ly/2EmhH8p" alt="WPP agrees to acquire majority stake in digital technology consultancy Cognifide" target="_blank" className="bg-black text-light px-1 mx-1">assisted in the selling of a large, 150 person digital firm based out of London</a> to the world’s largest digital agency, <a href="https://www.wpp.com/" alt="WPP" target="_blank" className="bg-black text-light px-1 mx-1">WPP</a>.
+              <li style={{ marginBottom: '1rem' }}>
+                In 2014, <a href="http://bit.ly/2EmhH8p" alt="WPP agrees to acquire majority stake in digital technology consultancy Cognifide" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;assisted in the selling of a large, 150 person digital firm based out of London&nbsp;</a> to the world’s largest digital agency, <a href="https://www.wpp.com/" alt="WPP" target="_blank" rel="noopener noreferrer" className="bg-black text-light mx-1">&nbsp;WPP&nbsp;</a>.
               </li>
-              <li>
+              <li style={{ marginBottom: '1rem' }}>
                 Assisted a Canadian publicly traded technology company in designing a mobile app with over 20 million downloads.
               </li>
             </ul>
