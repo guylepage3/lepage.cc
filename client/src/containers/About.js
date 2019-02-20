7fb0ec187d190be0dcbe893ac48bc6088f83ac04
Guy Lepage <guylepage3@gmail.com>
2019-02-20 16:28:48 -0500

    added to about page
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- client/src/containers/About.js
+++ client/src/containers/About.js
@@ -13,7 +13,7 @@ class About extends Component {
             backgroundColor: 'transparent'
         }}/>
         <NavLine />
-        <div className="container mb-8 pb-8">
+        <div className="container mb-8 pb-8 pr-9">
           <h1 className="h1-editorial">
             Simple, clean, concise  experiences that keep the user  coming back, time and time again.
           </h1>
@@ -27,7 +27,7 @@ class About extends Component {
             International award-winning senior product and UX/UI designer, marketing expert and entrepreneur with 19+ years experience working on some of the world’s largest brands and most exciting startups.
           </p>
           <p>
-          First hire at <a href="http://blockstack.org" alt="Blockstack" target="_blank">Blockstack</a>, a venture backed blockchain startup who has built a new decentralized internet for decentralized applications. Guy is an entrepreneur with a passion for business, design and technology. He is a former digital advertising agency business founder and is an internationally recognized designer. Guy took a non-traditional route and dropped out of College to pursue design and entrepreneurship. His career path has grown from a Newspaper Art Direction, to Creative Director with ad agencies and tech companies, to the pursuit and passion for software startups, design and technology.
+          First hire at <a href="http://blockstack.org" alt="Blockstack" target="_blank" className="bg-black text-light px-1 mx-1">Blockstack</a>, a venture backed blockchain startup who has built a new decentralized internet for decentralized applications. Guy is an entrepreneur with a passion for business, design and technology. He is a former digital advertising agency business founder and is an internationally recognized designer. Guy took a non-traditional route and dropped out of College to pursue design and entrepreneurship. His career path has grown from a Newspaper Art Direction, to Creative Director with ad agencies and tech companies, to the pursuit and passion for software startups, design and technology.
           </p>
           <h3 className="h3-editorial">
             Highlights
@@ -35,32 +35,32 @@ class About extends Component {
           <div>
             <ul>
               <li>
-                Was the first hire at <a href="http://blockstack.org" alt="Blockstack" target="_blank">Blockstack</a>, a blockchain startup working closely with the founders and designed and built 25+ products, including the world's first decentralized web browser, chrome extensions, native apps, web apps, websites, and more... All completed over a 2.5 yr period.
+                Was the first hire at <a href="http://blockstack.org" alt="Blockstack" target="_blank" className="bg-black text-light px-1 mx-1">Blockstack</a>, a blockchain startup working closely with the founders and designed and built 25+ products, including the world's first decentralized web browser, chrome extensions, native apps, web apps, websites, and more... All completed over a 2.5 yr period.
               </li>
               <li>
-                Was one of the core contributors to <a href="https://venturebeat.com/2017/12/04/blockstack-raises-52-million-to-build-a-parallel-internet-where-you-own-all-your-data/" alt="Blockstack raises $52 million to build a parallel internet where you own all your data" target="_blank">Blockstack’s $52M ICO</a> and token distribution. VC’s who participated in the ICO include;
+                Was one of the core contributors to <a href="https://venturebeat.com/2017/12/04/blockstack-raises-52-million-to-build-a-parallel-internet-where-you-own-all-your-data/" alt="Blockstack raises $52 million to build a parallel internet where you own all your data" target="_blank" className="bg-black text-light px-1 mx-1">Blockstack’s $52M ICO</a> and token distribution. VC’s who participated in the ICO include;
               </li>
                 <ul>
-                  <li><a href="http://usv.com" alt="Union Square Ventures (USV)" target="_blank">Union Square Ventures (USV)</a></li>
-                  <li><a href="http://foundationcapital.com/" alt="Foundation Capital" target="_blank">Foundation Capital</a></li>
-                  <li><a href="http://www.luxcapital.com/" alt="Lux Capital" target="_blank">Lux Capital</a></li>
-                  <li><a href="http://winklevosscapital.com/" alt="Winklevoss Capital" target="_blank">Winklevoss Capital</a></li>
-                  <li><a href="http://blockchain.capital/" alt="Blockchain Capital" target="_blank">Blockchain Capital</a></li>
-                  <li><a href="http://dcg.co/" alt="Digital Currency Group" target="_blank">Digital Currency Group</a></li>
-                  <li><a href="http://twitter.com/@kevinrose" alt="Kevin Rose" target="_blank">Kevin Rose</a></li>
-                  <li><a href="http://qasaryounis.com/" alt="Qasar Younis" target="_blank">Qasar Younis former COO of Y Combinator</a></li>
+                  <li><a href="http://usv.com" alt="Union Square Ventures (USV)" target="_blank" className="bg-black text-light px-1 mx-1">Union Square Ventures (USV)</a></li>
+                  <li><a href="http://foundationcapital.com/" alt="Foundation Capital" target="_blank" className="bg-black text-light px-1 mx-1">Foundation Capital</a></li>
+                  <li><a href="http://www.luxcapital.com/" alt="Lux Capital" target="_blank" className="bg-black text-light px-1 mx-1">Lux Capital</a></li>
+                  <li><a href="http://winklevosscapital.com/" alt="Winklevoss Capital" target="_blank" className="bg-black text-light px-1 mx-1">Winklevoss Capital</a></li>
+                  <li><a href="http://blockchain.capital/" alt="Blockchain Capital" target="_blank" className="bg-black text-light px-1 mx-1">Blockchain Capital</a></li>
+                  <li><a href="http://dcg.co/" alt="Digital Currency Group" target="_blank" className="bg-black text-light px-1 mx-1">Digital Currency Group</a></li>
+                  <li><a href="http://twitter.com/@kevinrose" alt="Kevin Rose" target="_blank" className="bg-black text-light px-1 mx-1">Kevin Rose</a></li>
+                  <li><a href="http://qasaryounis.com/" alt="Qasar Younis" target="_blank" className="bg-black text-light px-1 mx-1">Qasar Younis former COO of Y Combinator</a></li>
                 </ul>
               <li>
                 Was 1 of 4 core contributors in raising a $4M Series A funding round led by Union Square Ventures. Other investors include;
               </li>
                 <ul>
-                  <li><a href="http://www.luxcapital.com/" alt="Lux Capital" target="_blank">Lux Capital</a></li>
-                  <li><a href="http://dcg.co/" alt="Digital Currency Group" target="_blank">Digital Currency Group</a></li>
-                  <li><a href="http://compound.vc/" alt="Compound VC" target="_blank">Compound VC</a></li>
-                  <li><a href="http://versionone.vc/" alt="Version One Ventures" target="_blank">Version One Ventures</a></li>
-                  <li><a href="http://twitter.com/kalvepuri" alt="Kal Vepuri" target="_blank">Kal Vepuri</a></li>
-                  <li><a href="http://rtf.vc/" alt="Rising Tide" target="_blank">Rising Tide</a></li>
-                  <li><a href="https://twitter.com/naval" alt="Naval Ravikant (earlier investor)" target="_blank">Naval Ravikant (earlier investor)</a></li>
+                  <li><a href="http://www.luxcapital.com/" alt="Lux Capital" target="_blank" className="bg-black text-light px-1 mx-1">Lux Capital</a></li>
+                  <li><a href="http://dcg.co/" alt="Digital Currency Group" target="_blank" className="bg-black text-light px-1 mx-1">Digital Currency Group</a></li>
+                  <li><a href="http://compound.vc/" alt="Compound VC" target="_blank" className="bg-black text-light px-1 mx-1">Compound VC</a></li>
+                  <li><a href="http://versionone.vc/" alt="Version One Ventures" target="_blank" className="bg-black text-light px-1 mx-1">Version One Ventures</a></li>
+                  <li><a href="http://twitter.com/kalvepuri" alt="Kal Vepuri" target="_blank" className="bg-black text-light px-1 mx-1">Kal Vepuri</a></li>
+                  <li><a href="http://rtf.vc/" alt="Rising Tide" target="_blank" className="bg-black text-light px-1 mx-1">Rising Tide</a></li>
+                  <li><a href="https://twitter.com/naval" alt="Naval Ravikant (earlier investor)" target="_blank" className="bg-black text-light px-1 mx-1">Naval Ravikant (earlier investor)</a></li>
                 </ul>
               <li>
                 Assisted in growing the core blockchain user base from 35,000 to 75,000 users.
@@ -69,19 +69,19 @@ class About extends Component {
                 Was a key contributor in building a community of blockchain developers from 0 to 13,000 community members.
               </li>
               <li>
-                Guy sits on the Board of Advisors to <a href="https://news.net-cents.com/2018/03/21/blockstacks-guy-lepage-joins-netcents-technologys-board-advisors/" alt="NetCents Technology" target="_blank">NetCents Technology</a>, a publicly traded tech company trading in [Canada] and [Germany].
+                Guy sits on the Board of Advisors to <a href="https://news.net-cents.com/2018/03/21/blockstacks-guy-lepage-joins-netcents-technologys-board-advisors/" alt="NetCents Technology" target="_blank" className="bg-black text-light px-1 mx-1">NetCents Technology</a>, a publicly traded tech company trading in [Canada] and [Germany].
               </li>
               <li>
-                Designed a the worlds first decentralized web browser that was a finalist in the 2017 INDEX awards.
+                Designed a the worlds first decentralized web browser that was a <a href="https://twitter.com/guylepage3/status/889857953980964864" alt="Blockstack. A reinvention of the Internet for better privacy" target="_blank" className="bg-black text-light px-1 mx-1">finalist in the 2017 INDEX awards</a>.
               </li>
               <li>
-                Part of the New York / Silicon Valley Tech Startup community.
+                Part of the New York and Silicon Valley tech startup community.
               </li>
               <li>
-                In 2014 was hired to [assist in selling off 150 staff digital firm in London to the world’s largest digital agency WPP]. http://bit.ly/1iBCrUi.
+                In 2014 was hired to <a href="http://bit.ly/2EmhH8p" alt="WPP agrees to acquire majority stake in digital technology consultancy Cognifide" target="_blank" className="bg-black text-light px-1 mx-1">assist in selling off 150 staff digital firm in London to the world’s largest digital agency WPP</a>.
               </li>
               <li>
-                Have completed UX/UI design of 20million downloaded app for a publicly traded technology company.
+                Assisted a Canadian publicly traded technology company in designing a mobile app with over 20 million downloads.
               </li>
             </ul>
           </div>
